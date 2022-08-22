b1b1ff7ec25502cca88997ca944c3e233d7e5429
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-22 14:07:36 +0530

    GenericMethod.cs

---
+++ GenericMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace GenericDemo
+//{
+//    internal class GenericMethod
+//    {
+//        public static void toprint<T>(T[] inputArray)
+//        {
+//            foreach (var element in inputArray)
+//            {
+//                Console.WriteLine(element);
+//            }
+//            Console.WriteLine("------------");
+//        }
+
+//        public static void Main(string[] args)
+//        {
+//            int[] intArray = { 1, 2, 3, 4, 5 };
+//            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+//            char[] charArray = { 'R', 'I', 'Y', 'A', 's' };
+
+//            program.toprint<int>(intArray);
+//            program.toprint<double>(doubleArray);
+//            program.toprint<char>(charArray);
+//        }
+//    }
+//}
