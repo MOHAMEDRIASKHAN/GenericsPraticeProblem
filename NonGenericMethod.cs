a92207187bec9182eeb9caa5221f01eb5dc42b2a
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-22 14:08:47 +0530

    NonGenericMethod.cs

---
+++ NonGenericMethod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace GenericDemo
+//{
+//    class program
+//    {
+//        public static void toprint(int[] inputArray)
+//        {
+//            foreach (int element in inputArray)
+//            {
+//                Console.WriteLine(element);
+//            }
+//            Console.WriteLine("------------");
+//        }
+
+//        public static void toprint(double[] inputArray)
+//        {
+//            foreach (double element in inputArray)
+//            {
+//                Console.WriteLine(element);
+//            }
+//            Console.WriteLine("------------");
+//        }
+
+//        public static void toprint(char[] inputArray)
+//        {
+//            foreach (char element in inputArray)
+//            {
+//                Console.WriteLine(element);
+//            }
+//            Console.WriteLine("------------");
+//        }
+
+//        public static void Main(string[] args)
+//        {
+//            int[] intArray = { 1, 2, 3, 4, 5 };
+//            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+//            char[] charArray = { 'R', 'I', 'Y', 'A', 's' };
+
+//            program.toprint(intArray);
+//            program.toprint(doubleArray);
+//            program.toprint(charArray);
+//        }
+//    }
+//}
+
+
